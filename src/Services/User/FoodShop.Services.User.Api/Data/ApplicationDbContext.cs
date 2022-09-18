440c0976b464f286d25038d96bb93e7b2060f451
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-09-18 19:09:31 +0300

    - Initialized database configuration and initialization for FoodShop.Services.User.Api. Also tested that connection string works

---
+++ src/Services/User/FoodShop.Services.User.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FoodShop.Services.User.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodShop.Services.User.Api.Data
+{
+    public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
--- src/Services/User/FoodShop.Services.User.Api/Program.cs
+++ src/Services/User/FoodShop.Services.User.Api/Program.cs
@@ -1,6 +1,20 @@
+using FoodShop.Services.User.Api.Data;
+using FoodShop.Services.User.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

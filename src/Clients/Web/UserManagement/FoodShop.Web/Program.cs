6eb18767d58ae7d65aa4cb6e8e0f53c7b321049f
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-09-22 01:17:24 +0300

    - Added User Api and Identity Api reference to user web client management app - Created api clients for both of those resource servers

--- src/Clients/Web/UserManagement/FoodShop.Web/Program.cs
+++ src/Clients/Web/UserManagement/FoodShop.Web/Program.cs
@@ -1,9 +1,35 @@
+using FoodShop.Web.User.Services;
+using FoodShop.Web.User.Services.Contracts;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddTransient<JwtTokenHeaderHandlerService>();
+
+builder.Services.AddHttpClient("IdentityServerApiClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ServiceUrls:IdentityServerApi"]);
+});
+
+builder.Services.AddScoped<IUserApiService, UserApiService>();
+
+builder.Services.AddHttpClient<IUserApiService, UserApiService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ServiceUrls:UserManagementApi"]);
+})
+    .AddHttpMessageHandler<JwtTokenHeaderHandlerService>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddAuthentication(options =>
 {
@@ -13,10 +39,12 @@ builder.Services.AddAuthentication(options =>
     .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, config =>
     {
         config.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+        config.AccessDeniedPath = "/Authorization/Index";
     })
     .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
     {
-        options.Authority = builder.Configuration["ServiceUrls:IdentityApi"];
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.Authority = builder.Configuration["ServiceUrls:IdentityServerApi"];
         options.GetClaimsFromUserInfoEndpoint = true;
         options.ClientId = "foodShop-User-Management-Admin";
         options.ClientSecret = "49C1A7E1-0C79-4A89-A3D6-A37998FB86B0";
@@ -27,16 +55,17 @@ builder.Services.AddAuthentication(options =>
         options.Scope.Add("foodShop.user.create");
         options.Scope.Add("foodShop.user.delete");
         options.Scope.Add("foodShop.user.update");
+        options.ClaimActions.MapUniqueJsonKey("role", "role");
         options.SaveTokens = true;
     });
-
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    IdentityModelEventSource.ShowPII = true;
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -47,7 +76,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(

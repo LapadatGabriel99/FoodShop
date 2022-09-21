a2843ccff43be9e53f85295044fa5273bb660f2d
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-09-22 01:19:23 +0300

    - Added more claims inside the db for an admin user - Added more admin users in the db seeder service

--- src/Services/Identity/FoodShop.Services.Identity.Api/Services/DbSeederService.cs
+++ src/Services/Identity/FoodShop.Services.Identity.Api/Services/DbSeederService.cs
@@ -21,9 +21,9 @@ namespace FoodShop.Services.Identity.Api.Services
 
         public async Task SeedDatabaseAsync()
         {
-            var adminRole = await _roleManager.FindByNameAsync(Role.UserMangementAdmin);
+            var userAdminRole = await _roleManager.FindByNameAsync(Role.UserMangementAdmin);
 
-            if (adminRole != null)
+            if (userAdminRole != null)
             {
                 return;
             }
@@ -37,7 +37,9 @@ namespace FoodShop.Services.Identity.Api.Services
                 EmailConfirmed = true,
                 PhoneNumber = "07707721654",
                 FirstName = "Gabi",
-                LastName = "Lapadat"
+                LastName = "Lapadat",
+                Address = "Romania, Craiova, Dolj, Str Simion Stoilov nr 12, bl E9",
+                PhoneNumberConfirmed = true,
             };
 
             await _userManager.CreateAsync(adminUser, "Admin123*");
@@ -47,7 +49,57 @@ namespace FoodShop.Services.Identity.Api.Services
                 new Claim(JwtClaimTypes.Name, adminUser.FirstName + " " + adminUser.LastName),
                 new Claim(JwtClaimTypes.GivenName, adminUser.FirstName),
                 new Claim(JwtClaimTypes.FamilyName, adminUser.LastName),
-                new Claim(JwtClaimTypes.Role, Role.UserMangementAdmin)
+                new Claim(JwtClaimTypes.Role, Role.UserMangementAdmin),
+                new Claim(JwtClaimTypes.Address, adminUser.Address),
+                new Claim(JwtClaimTypes.PhoneNumber, adminUser.PhoneNumber)
+            });
+
+            var adminUser2 = new ApplicationUser()
+            {
+                UserName = "UserAdmin2",
+                Email = "userAdmin2@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "07707721654",
+                FirstName = "Alex",
+                LastName = "Popescu",
+                Address = "Romania, Craiova, Dolj, Str Simion Stoilov nr 12, bl E10",
+                PhoneNumberConfirmed = true,
+            };
+
+            await _userManager.CreateAsync(adminUser2, "Admin123*");
+            await _userManager.AddToRoleAsync(adminUser2, Role.UserMangementAdmin);
+            await _userManager.AddClaimsAsync(adminUser2, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, adminUser2.FirstName + " " + adminUser2.LastName),
+                new Claim(JwtClaimTypes.GivenName, adminUser2.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, adminUser2.LastName),
+                new Claim(JwtClaimTypes.Role, Role.UserMangementAdmin),
+                new Claim(JwtClaimTypes.Address, adminUser2.Address),
+                new Claim(JwtClaimTypes.PhoneNumber, adminUser2.PhoneNumber)
+            });
+
+            var adminUser3 = new ApplicationUser()
+            {
+                UserName = "UserAdmin3",
+                Email = "userAdmin3@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "07707721654",
+                FirstName = "Robert",
+                LastName = "Dorobantu",
+                Address = "Romania, Craiova, Dolj, Str Simion Stoilov nr 12, bl E10",
+                PhoneNumberConfirmed = true,
+            };
+
+            await _userManager.CreateAsync(adminUser3, "Admin123*");
+            await _userManager.AddToRoleAsync(adminUser3, Role.UserMangementAdmin);
+            await _userManager.AddClaimsAsync(adminUser3, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, adminUser3.FirstName + " " + adminUser3.LastName),
+                new Claim(JwtClaimTypes.GivenName, adminUser3.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, adminUser3.LastName),
+                new Claim(JwtClaimTypes.Role, Role.UserMangementAdmin),
+                new Claim(JwtClaimTypes.Address, adminUser3.Address),
+                new Claim(JwtClaimTypes.PhoneNumber, adminUser3.PhoneNumber)
             });
         }
     }

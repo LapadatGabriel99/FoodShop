047f6c5dcf0e79cd8f09c8b79db3f22741c65946
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-09-23 02:54:42 +0300

    - Added EditController, - User Details page, user edit page etc

---
+++ src/Clients/Web/UserManagement/FoodShop.Web/Controllers/EditController.cs
@@ -0,0 +1,51 @@
+﻿using FoodShop.Web.User.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodShop.Web.User.Controllers
+{
+    [Authorize(Roles = Role.UserMangementAdmin)]
+    public class EditUserController : Controller
+    {
+        private readonly ILogger<EditUserController> _logger;
+        private readonly IUserService _userService;
+
+        public EditUserController(ILogger<EditUserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var userDetails = await _userService.GetById("api/user/get", id);
+
+            return View(userDetails);
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword(string id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ChangeEmail(string id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ChangePhoneNumber(string id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ChangeBasicCredentials(string id)
+        {
+            return View();
+        }
+    }
+}

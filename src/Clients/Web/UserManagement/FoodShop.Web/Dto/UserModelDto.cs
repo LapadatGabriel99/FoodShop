2e7ab695d3968381a98d979125cddf432dbd59c6
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-09-23 02:16:05 +0300

    - UserModelDto now contains also confirm password property

--- src/Clients/Web/UserManagement/FoodShop.Web/Dto/UserModelDto.cs
+++ src/Clients/Web/UserManagement/FoodShop.Web/Dto/UserModelDto.cs
@@ -8,6 +8,8 @@
 
         public string Password { get; set; }
 
+        public string ConfirmPassword { get; set; }
+
         public string Role { get; set; }
 
         public string Email { get; set; }

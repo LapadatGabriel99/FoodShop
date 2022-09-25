9e4abb9d1e3c93d067b6fbc94a56e2bc73791f68
Gabi Lapadat <lapadatrobert123@gmail.com>
2022-09-26 01:56:56 +0300

    - Added global exception filter layer

---
+++ src/Services/User/FoodShop.Services.User.Api/Web/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace FoodShop.Services.User.Api.Web.Filters
+{
+    public sealed class GlobalExceptionFilter : IAsyncExceptionFilter
+    {
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            Debug.WriteLine(context.Exception);
+
+            context.Result = new BadRequestObjectResult(context.Exception);
+        }
+    }
+}
